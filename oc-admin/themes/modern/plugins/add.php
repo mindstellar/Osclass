6fcff82852236809fd30a7a7f43cf540c0e3b5fb
webmods-croatia <53878055+webmods-croatia@users.noreply.github.com>
2019-08-13 15:59:41 +0200

    Update add.php
    Removing all ties with Osclass Market.

--- oc-admin/themes/modern/plugins/add.php
+++ oc-admin/themes/modern/plugins/add.php
@@ -68,41 +68,5 @@
         <?php } ?>
         </div>
     </div>
-
-    <div id="market_installer" class="has-form-actions hide">
-        <form action="" method="post">
-            <input type="hidden" name="market_code" id="market_code" value="" />
-            <div class="osc-modal-content-market">
-                <img src="" id="market_thumb" class="float-left"/>
-                <table class="table" cellpadding="0" cellspacing="0">
-                    <tbody>
-                        <tr class="table-first-row">
-                            <td><?php _e('Name'); ?></td>
-                            <td><span id="market_name"><?php _e("Loading data"); ?></span></td>
-                        </tr>
-                        <tr class="even">
-                            <td><?php _e('Version'); ?></td>
-                            <td><span id="market_version"><?php _e("Loading data"); ?></span></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('Author'); ?></td>
-                            <td><span id="market_author"><?php _e("Loading data"); ?></span></td>
-                        </tr>
-                        <tr class="even">
-                            <td><?php _e('URL'); ?></td>
-                            <td><span id="market_url_span"><a id="market_url" href="#"><?php _e("Download manually"); ?></a></span></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div class="clear"></div>
-            </div>
-            <div class="form-actions">
-                <div class="wrapper">
-                    <button id="market_cancel" class="btn btn-red" ><?php _e('Cancel'); ?></button>
-                    <button id="market_install" class="btn btn-submit" ><?php _e('Continue install'); ?></button>
-                </div>
-            </div>
-        </form>
-    </div>
 </div>
 <?php osc_current_admin_theme_path('parts/footer.php'); ?>
