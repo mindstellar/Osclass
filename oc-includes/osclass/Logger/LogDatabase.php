ad3aaa2983f1ae7b6b1a360fbb5b2b71680e23ef
Navjot Tomer <navjottomer@gmail.com>
2019-08-27 12:25:47 +0530

    CS-FIxer

--- oc-includes/osclass/Logger/LogDatabase.php
+++ oc-includes/osclass/Logger/LogDatabase.php
@@ -1,5 +1,5 @@
-<?php if ( ! defined( 'ABS_PATH' ) ) {
-	exit( 'ABS_PATH is not loaded. Direct access is not allowed.' );
+<?php if (! defined('ABS_PATH')) {
+    exit('ABS_PATH is not loaded. Direct access is not allowed.');
 }
 
 /*
@@ -39,13 +39,13 @@
          */
         public $explain_messages;
 
-	    /**
-	     *
-	     * @return \LogDatabase
-	     */
+        /**
+         *
+         * @return \LogDatabase
+         */
         public static function newInstance()
         {
-            if( !self::$instance instanceof self ) {
+            if (!self::$instance instanceof self) {
                 self::$instance = new self;
             }
             return self::$instance;
@@ -54,7 +54,7 @@
         /**
          *
          */
-        public function _construct()
+        public function __construct()
         {
             $this->messages         = array();
             $this->explain_messages = array();
@@ -77,11 +77,11 @@
             );
         }
 
-	    /**
-	     *
-	     * @param $sql
-	     * @param      $results
-	     */
+        /**
+         *
+         * @param $sql
+         * @param      $results
+         */
         public function addExplainMessage($sql, $results)
         {
             $this->explain_messages[] = array(
@@ -98,18 +98,18 @@
             echo '<fieldset style="border:1px solid black; padding:6px 10px 10px 10px; margin: 20px; width: 95%; background-color: #FFFFFF;" >' . PHP_EOL;
             echo '<legend style="font-size: 16px;">&nbsp;&nbsp;Database queries (Total queries: ' . $this->getTotalNumberQueries() .' - Total queries time: ' . $this->getTotalQueriesTime() . ')&nbsp;&nbsp;</legend>' . PHP_EOL;
             echo '<table style="border-collapse: separate; *border-collapse: collapse; width: 100%; font-size: 13px; padding: 15px; border-spacing: 0;">' . PHP_EOL;
-            if( count($this->messages) == 0 ) {
+            if (count($this->messages) == 0) {
                 echo '<tr><td>No queries</td></tr>' . PHP_EOL;
             } else {
-                foreach($this->messages as $msg) {
+                foreach ($this->messages as $msg) {
                     $row_style = '';
-                    if( $msg['errno'] != 0 ) {
+                    if ($msg['errno'] != 0) {
                         $row_style = 'style="background-color: #FFC2C2;"';
                     }
                     echo '<tr ' . $row_style . '>' . PHP_EOL;
                     echo '<td style="padding: 10px 10px 9px; text-align: left; vertical-align: top; border: 1px solid #ddd;">' . $msg['query_time'] . '</td>' . PHP_EOL;
                     echo '<td style="padding: 10px 10px 9px; text-align: left; vertical-align: top; border: 1px solid #ddd;">';
-                    if( $msg['errno'] != 0 ) {
+                    if ($msg['errno'] != 0) {
                         echo '<strong>Error number:</strong> ' . $msg['errno'] . '<br/>';
                         echo '<strong>Error description:</strong> ' . $msg['error'] . '<br/><br/>';
                     }
@@ -122,36 +122,36 @@
             echo '</fieldset>' . PHP_EOL;
         }
 
-	    /**
-	     * @return bool
-	     */
-	    public function writeMessages()
+        /**
+         * @return bool
+         */
+        public function writeMessages()
         {
             $filename = CONTENT_PATH . 'queries.log';
 
-            if( !file_exists($filename) || !is_writable($filename) ) {
+            if (!file_exists($filename) || !is_writable($filename)) {
                 return false;
             }
 
-	        $fp = fopen( $filename , 'ab' );
+            $fp = fopen($filename, 'ab');
 
-            if( $fp == false ) {
+            if ($fp == false) {
                 return false;
             }
 
             fwrite($fp, '==================================================' . PHP_EOL);
-            if(MULTISITE) {
-                fwrite($fp, '=' . str_pad('Date: ' . date('Y-m-d').' '.date('H:i:s'), 48, ' ' , STR_PAD_BOTH) . '=' . PHP_EOL);
+            if (MULTISITE) {
+                fwrite($fp, '=' . str_pad('Date: ' . date('Y-m-d').' '.date('H:i:s'), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
             } else {
-                fwrite($fp, '=' . str_pad('Date: ' . date(osc_date_format()!=''?osc_date_format():'Y-m-d').' '.date(osc_time_format()!=''?osc_date_format():'H:i:s'), 48, ' ' , STR_PAD_BOTH) . '=' . PHP_EOL);
+                fwrite($fp, '=' . str_pad('Date: ' . date(osc_date_format()!=''?osc_date_format():'Y-m-d').' '.date(osc_time_format()!=''?osc_date_format():'H:i:s'), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
             }
-            fwrite($fp, '=' . str_pad('Total queries: ' . $this->getTotalNumberQueries(), 48, ' ' , STR_PAD_BOTH) . '=' . PHP_EOL);
-            fwrite($fp, '=' . str_pad('Total queries time: ' . $this->getTotalQueriesTime(), 48, ' ' , STR_PAD_BOTH) . '=' . PHP_EOL);
+            fwrite($fp, '=' . str_pad('Total queries: ' . $this->getTotalNumberQueries(), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
+            fwrite($fp, '=' . str_pad('Total queries time: ' . $this->getTotalQueriesTime(), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
             fwrite($fp, '==================================================' . PHP_EOL . PHP_EOL);
 
-            foreach($this->messages as $msg) {
+            foreach ($this->messages as $msg) {
                 fwrite($fp, 'QUERY TIME' . ' ' . $msg['query_time'] . PHP_EOL);
-                if( $msg['errno'] != 0 ) {
+                if ($msg['errno'] != 0) {
                     fwrite($fp, 'Error number: ' . $msg['errno'] . PHP_EOL);
                     fwrite($fp, 'Error description: ' . $msg['error'] . PHP_EOL);
                 }
@@ -165,62 +165,62 @@
             return true;
         }
 
-	    /**
-	     * @return bool
-	     */
-	    public function writeExplainMessages()
+        /**
+         * @return bool
+         */
+        public function writeExplainMessages()
         {
             $filename = CONTENT_PATH . 'explain_queries.log';
 
-            if( !file_exists($filename) || !is_writable($filename) ) {
+            if (!file_exists($filename) || !is_writable($filename)) {
                 return false;
             }
 
-	        $fp = fopen( $filename , 'ab' );
+            $fp = fopen($filename, 'ab');
 
-            if( $fp == false ) {
+            if ($fp == false) {
                 return false;
             }
 
             fwrite($fp, '==================================================' . PHP_EOL);
-            if(MULTISITE) {
-                fwrite($fp, '=' . str_pad('Date: ' . date('Y-m-d').' '.date('H:i:s'), 48, ' ' , STR_PAD_BOTH) . '=' . PHP_EOL);
+            if (MULTISITE) {
+                fwrite($fp, '=' . str_pad('Date: ' . date('Y-m-d').' '.date('H:i:s'), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
             } else {
-	            fwrite( $fp , '=' . str_pad( 'Date: ' . date( osc_date_format() ?: 'Y-m-d' ) . ' ' . date( osc_time_format() ?: 'H:i:s' ) , 48 , ' ' , STR_PAD_BOTH ) . '=' . PHP_EOL );
+                fwrite($fp, '=' . str_pad('Date: ' . date(osc_date_format() ?: 'Y-m-d') . ' ' . date(osc_time_format() ?: 'H:i:s'), 48, ' ', STR_PAD_BOTH) . '=' . PHP_EOL);
             }
             fwrite($fp, '==================================================' . PHP_EOL . PHP_EOL);
 
-            $title  = '|' . str_pad( 'id', 3, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'select_type', 20, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'table', 20, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'type', 8, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'possible_keys', 28, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'key', 18, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'key_len', 9, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'ref', 48, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'rows', 8, ' ' , STR_PAD_BOTH) . '|';
-            $title .= str_pad( 'Extra', 38, ' ' , STR_PAD_BOTH) . '|';
+            $title  = '|' . str_pad('id', 3, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('select_type', 20, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('table', 20, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('type', 8, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('possible_keys', 28, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('key', 18, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('key_len', 9, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('ref', 48, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('rows', 8, ' ', STR_PAD_BOTH) . '|';
+            $title .= str_pad('Extra', 38, ' ', STR_PAD_BOTH) . '|';
 
-	        for ( $i = 0 , $iMax = count( $this->explain_messages ); $i < $iMax; $i ++ ) {
+            for ($i = 0 , $iMax = count($this->explain_messages); $i < $iMax; $i ++) {
                 fwrite($fp, $this->explain_messages[$i]['query'] . PHP_EOL);
-                fwrite($fp, str_pad( '', 211, '-' , STR_PAD_BOTH) . PHP_EOL);
+                fwrite($fp, str_pad('', 211, '-', STR_PAD_BOTH) . PHP_EOL);
                 fwrite($fp, $title . PHP_EOL);
-                fwrite($fp, str_pad( '', 211, '-' , STR_PAD_BOTH) . PHP_EOL);
-                foreach($this->explain_messages[$i]['explain'] as $explain) {
-                    $row  = '|' . str_pad( $explain['id'], 3, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['select_type'], 20, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['table'], 20, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['type'], 8, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['possible_keys'], 28, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['key'], 18, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['key_len'], 9, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['ref'], 48, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['rows'], 8, ' ' , STR_PAD_BOTH) . '|';
-                    $row .= str_pad( $explain['Extra'], 38, ' ' , STR_PAD_BOTH) . '|';
+                fwrite($fp, str_pad('', 211, '-', STR_PAD_BOTH) . PHP_EOL);
+                foreach ($this->explain_messages[$i]['explain'] as $explain) {
+                    $row  = '|' . str_pad($explain['id'], 3, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['select_type'], 20, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['table'], 20, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['type'], 8, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['possible_keys'], 28, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['key'], 18, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['key_len'], 9, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['ref'], 48, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['rows'], 8, ' ', STR_PAD_BOTH) . '|';
+                    $row .= str_pad($explain['Extra'], 38, ' ', STR_PAD_BOTH) . '|';
                     fwrite($fp, $row . PHP_EOL);
-                    fwrite($fp, str_pad( '', 211, '-' , STR_PAD_BOTH) . PHP_EOL);
+                    fwrite($fp, str_pad('', 211, '-', STR_PAD_BOTH) . PHP_EOL);
                 }
-                if( $i != ( count($this->explain_messages) - 1 ) ) {
+                if ($i != (count($this->explain_messages) - 1)) {
                     fwrite($fp, PHP_EOL . PHP_EOL);
                 }
             }
@@ -230,27 +230,26 @@
             return true;
         }
 
-	    /**
-	     * @return int
-	     */
-	    public function getTotalQueriesTime()
+        /**
+         * @return int
+         */
+        public function getTotalQueriesTime()
         {
             $time = 0;
-            foreach($this->messages as $m) {
-	            $time += $m[ 'query_time' ];
+            foreach ($this->messages as $m) {
+                $time += $m[ 'query_time' ];
             }
 
             return $time;
         }
 
-	    /**
-	     * @return int
-	     */
-	    public function getTotalNumberQueries()
+        /**
+         * @return int
+         */
+        public function getTotalNumberQueries()
         {
             return count($this->messages);
         }
     }
 
     /* file end: ./oc-includes/osclass/Logger/LogDatabase.php */
-
