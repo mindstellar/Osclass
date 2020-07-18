7777ca09dd709d804f5713d5839738891d9b446b
Navjot Tomer <navjottomer@gmail.com>
2020-07-19 01:38:48 +0530

    #242

--- oc-includes/osclass/utils.php
+++ oc-includes/osclass/utils.php
@@ -1156,27 +1156,13 @@ function rglob($pattern, $flags = 0, $path = '')
         if ($dir === '\\' || $dir === '/') {
             $dir = '';
         }
-
         return rglob(basename($pattern), $flags, $dir . '/');
     }
-    /**
-     *
-     * $paths = glob($path . '*', GLOB_ONLYDIR | GLOB_NOSORT);
-     * $files = glob($path . $pattern, $flags);
-     * foreach ($paths as $p) {
-     * $files = array_merge($files, rglob($pattern, $flags, $p . '/'));
-     * }
-     */
-    //TODO
-    //Better Maybe ?
-
     $paths = glob($path . '*', GLOB_ONLYDIR | GLOB_NOSORT);
     $files = glob($path . $pattern, $flags);
     foreach ($paths as $p) {
-        $files[] = rglob($pattern, $flags, $p . '/');
+        $files = array_merge($files, rglob($pattern, $flags, $p . '/'));
     }
-    $files = array_merge([], ...$files);
-
     return $files;
 }
 
