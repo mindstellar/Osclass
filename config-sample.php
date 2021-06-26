9001f5e16b4b02ddd9fc06bfebc560613b06c737
Navjot Tomer <navjottomer@gmail.com>
2021-06-26 15:54:15 +0530

    Docs: update

--- config-sample.php
+++ config-sample.php
@@ -1,7 +1,18 @@
 <?php
-
 /**
- * The base MySQL settings of Osclass
+ * The base configuration for Osclass
+ *
+ * The config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Database table prefix
+ *
+ *
+ * @package Osclass
  */
 
 /** MySQL database name for Osclass */
@@ -19,6 +30,8 @@ define('DB_HOST', 'localhost');
 /** Database Table prefix */
 define('DB_TABLE_PREFIX', 'oc_');
 
+/** Website relative root path */
 define('REL_WEB_URL', 'rel_here');
 
+/** Website base url */
 define('WEB_PATH', 'http://localhost');
