6b4bae8fb77ba35e996c97b60f86e496b1538260
Navjot Tomer <navjottomer@gmail.com>
2021-06-29 08:28:48 +0530

    Build: composer update

--- oc-includes/vendor/composer/autoload_real.php
+++ oc-includes/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit1fcba07043a631f012853cd02fe384cd
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1fcba07043a631f012853cd02fe384cd', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit1fcba07043a631f012853cd02fe384cd', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit1fcba07043a631f012853cd02fe384cd::getInitializer($loader));
         } else {
