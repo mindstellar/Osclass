c213cc31b7b96decc8d50a3971cda40edf7254ef
navjottomer <navjottomer@gmail.com>
2021-08-01 10:25:56 +0530

    Improvement: Removed Jquery-ui modal with bootstrap modal and pure javascript in category/index.php

--- oc-admin/themes/modern/categories/index.php
+++ oc-admin/themes/modern/categories/index.php
@@ -37,10 +37,10 @@ $categories = __get('categories');
 function addHelp()
 {
     echo '<p>'
-         . __('Add, edit or delete the categories or subcategories in which users can post listings. '
-              . 'Reorder sections by dragging and dropping, or nest a subcategory in an expanded category. '
-              . '<strong>Be careful</strong>: If you delete a category, all listings associated will also be deleted!')
-         . '</p>';
+        . __('Add, edit or delete the categories or subcategories in which users can post listings. '
+            . 'Reorder sections by dragging and dropping, or nest a subcategory in an expanded category. '
+            . '<strong>Be careful</strong>: If you delete a category, all listings associated will also be deleted!')
+        . '</p>';
 }
 
 
@@ -48,14 +48,13 @@ osc_add_hook('help_box', 'addHelp');
 
 function customPageHeader()
 {
-    ?>
+?>
     <h1><?php _e('Categories'); ?>
         <a href="#" class="ms-1 bi bi-question-circle-fill float-right" data-bs-target="#help-box" data-bs-toggle="collapse"></a>
-        <a href="<?php echo osc_admin_base_url(true); ?>?page=categories&amp;action=add_post_default&<?php echo osc_csrf_token_url(); ?>"
-           class="text-success ms-1 float-end" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php _e('Add'); ?>"><i class="bi
+        <a href="<?php echo osc_admin_base_url(true); ?>?page=categories&amp;action=add_post_default&<?php echo osc_csrf_token_url(); ?>" class="text-success ms-1 float-end" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php _e('Add'); ?>"><i class="bi
            bi-plus-circle-fill"></i></a>
     </h1>
-    <?php
+<?php
 }
 
 
@@ -77,7 +76,7 @@ osc_add_filter('admin_title', 'customPageTitle');
 //customize Head
 function customHead()
 {
-    ?>
+?>
     <script type="text/javascript">
         document.write('<style type="text/css">.tabber{ display:none; }</style>');
     </script>
@@ -111,10 +110,10 @@ function customHead()
         }
     </style>
     <script type="text/javascript">
-        $(function () {
-            $('.category_div').on('mouseenter', function () {
+        $(function() {
+            $('.category_div').on('mouseenter', function() {
                 $(this).addClass('cat-hover');
-            }).on('mouseleave', function () {
+            }).on('mouseleave', function() {
                 $(this).removeClass('cat-hover');
             });
             var list_original = '';
@@ -133,19 +132,18 @@ function customHead()
                 tabSize: 25,
                 tolerance: 'pointer',
                 toleranceElement: '> div',
-                create: function (event, ui) {
-                },
-                start: function (event, ui) {
+                create: function(event, ui) {},
+                start: function(event, ui) {
                     list_original = $('.sortable').nestedSortable('serialize');
                     $(ui.helper).addClass('footest');
                     $(ui.helper).prepend('<div style="opacity: 1 !important; padding:5px;" class="alert-custom"><?php echo osc_esc_js(__('Note: You must expand the category in order to make it a subcategory.')); ?></div>');
                 },
-                stop: function (event, ui) {
+                stop: function(event, ui) {
 
                     var plist;
                     $(".jsMessage").fadeIn("fast");
                     $(".jsMessage p").attr('class', '');
-                    $(".jsMessage p").html('<img height="16" width="16" src="<?php echo osc_current_admin_theme_url('images/loading.gif');?>"> <?php echo osc_esc_js(__('This action could take a while.')); ?>');
+                    $(".jsMessage p").html('<img height="16" width="16" src="<?php echo osc_current_admin_theme_url('images/loading.gif'); ?>"> <?php echo osc_esc_js(__('This action could take a while.')); ?>');
 
                     var list = '';
                     list = $('.sortable').nestedSortable('serialize');
@@ -163,18 +161,22 @@ function customHead()
                         $(ui.item).parent().addClass('subcategory list-unstyled');
                     }
                     if (list_original !== list) {
-                        plist = array_list.reduce(function (total, current, index) {
-                            total[index] = {'c': current.id, 'p': current.parent_id};
+                        plist = array_list.reduce(function(total, current, index) {
+                            total[index] = {
+                                'c': current.id,
+                                'p': current.parent_id
+                            };
                             return total;
                         }, {});
                         //console.log(plist);
                         $.ajax({
                             type: 'POST',
-                            url: "<?php echo osc_admin_base_url(true) . '?page=ajax&action=categories_order&'
-                                             . osc_csrf_token_url(); ?>",
-                            data: {'list': JSON.stringify(plist)},
+                            url: "<?php echo osc_admin_base_url(true) . '?page=ajax&action=categories_order&' . osc_csrf_token_url(); ?>",
+                            data: {
+                                'list': JSON.stringify(plist)
+                            },
                             context: document.body,
-                            success: function (res) {
+                            success: function(res) {
                                 var ret = eval("(" + res + ")");
                                 var message = "";
                                 if (ret.error) {
@@ -189,7 +191,7 @@ function customHead()
                                 $(".jsMessage").show();
                                 $(".jsMessage p").html(message);
                             },
-                            error: function () {
+                            error: function() {
                                 $(".jsMessage").fadeIn("fast");
                                 $(".jsMessage p").attr('class', '');
                                 $(".jsMessage p").html('<?php echo osc_esc_js(__('Ajax error, please try again.')); ?>');
@@ -201,7 +203,7 @@ function customHead()
                 }
             });
 
-            $(".toggle").bind("click", function (e) {
+            $(".toggle").bind("click", function(e) {
                 var list = $(this).parents('li').first().find('ul');
                 var lili = $(this).closest('li').find('ul').find('li').find('ul');
                 var li = $(this).closest('li').first();
@@ -216,49 +218,6 @@ function customHead()
                     $(this).removeClass('bi-chevron-down').addClass('bi-chevron-right');
                 }
             });
-
-            // dialog delete
-            $("#dialog-delete-category").dialog({
-                autoOpen: false,
-                modal: true
-            });
-            $("#category-delete-submit").click(function () {
-                var id = $("#dialog-delete-category").attr('data-category-id');
-                var url = '<?php echo osc_admin_base_url(true); ?>?page=ajax&action=delete_category&<?php echo osc_csrf_token_url(); ?>&id=' + id;
-
-                $.ajax({
-                    url: url,
-                    context: document.body,
-                    success: function (res) {
-                        var ret = eval("(" + res + ")");
-                        var message = "";
-                        if (ret.error) {
-                            message += ret.error;
-                            $(".jsMessage p").attr('class', 'error');
-                        }
-                        if (ret.ok) {
-                            message += ret.ok;
-                            $(".jsMessage p").attr('class', 'ok');
-
-                            $('#list_' + id).fadeOut("slow")
-                                .remove();
-                        }
-
-                        $(".jsMessage").show();
-                        $(".jsMessage p").html(message);
-                    },
-                    error: function () {
-                        $(".jsMessage").show();
-                        $(".jsMessage p").attr('class', '');
-                        $(".jsMessage p").html("<?php echo osc_esc_js(__('Ajax error, try again.')); ?>");
-                    }
-                });
-                $('#dialog-delete-category').dialog('close');
-                $('body,html').animate({
-                    scrollTop: 0
-                }, 500);
-                return false;
-            });
         });
 
         list_original = $('.sortable').nestedSortable('serialize');
@@ -272,7 +231,7 @@ function customHead()
                 $.ajax({
                     url: url,
                     context: document.body,
-                    success: function (res) {
+                    success: function(res) {
                         $('div.' + class_name).html(res);
                         $('div.' + class_name).fadeIn("fast");
                     }
@@ -280,12 +239,6 @@ function customHead()
             } else {
                 $('.iframe-category').remove();
             }
-            return false;
-        }
-
-        function delete_category(id) {
-            $("#dialog-delete-category").attr('data-category-id', id);
-            $("#dialog-delete-category").dialog('open');
             return false;
         }
 
@@ -298,8 +251,8 @@ function customHead()
 
             jMessage.fadeIn("fast");
             jMessageP.attr('class', '');
-            jMessageP.html('<img height="16" width="16" src="<?php echo osc_current_admin_theme_url('images/loading.gif');?>"> <?php echo
-            osc_esc_js(__('This action could take a while.')); ?>');
+            jMessageP.html('<img height="16" width="16" src="<?php echo osc_current_admin_theme_url('images/loading.gif'); ?>"> <?php echo
+                                                                                                                                osc_esc_js(__('This action could take a while.')); ?>');
 
             if ($(category).hasClass('disabled')) {
                 enabled = 1;
@@ -311,7 +264,7 @@ function customHead()
             $.ajax({
                 url: url,
                 context: document.body,
-                success: function (res) {
+                success: function(res) {
                     const ret = eval("(" + res + ")");
                     let message = "";
 
@@ -365,7 +318,7 @@ function customHead()
                     jMessage.show();
                     jMessageP.html(message);
                 },
-                error: function () {
+                error: function() {
                     jMessage.show();
                     jMessageP.attr('class', '');
                     jMessageP.html("<?php echo osc_esc_js(__('Ajax error, try again.')); ?>");
@@ -373,7 +326,7 @@ function customHead()
             });
         }
     </script>
-    <?php
+<?php
 }
 
 
@@ -388,12 +341,9 @@ function drawCategory($category)
         $has_subcategories = true;
     } else {
         $has_subcategories = false;
-    }
-    ?>
-    <li id="list_<?php echo $category['pk_i_id']; ?>"
-        class="category_li <?php echo($category['b_enabled'] == 1 ? 'enabled' : 'disabled'); ?> ">
-        <div class="<?php echo($category['b_enabled'] == 1 ? 'enabled' : 'disabled'); ?>"
-             category_id="<?php echo $category['pk_i_id']; ?>">
+    } ?>
+    <li id="list_<?php echo $category['pk_i_id']; ?>" class="category_li <?php echo ($category['b_enabled'] == 1 ? 'enabled' : 'disabled'); ?> ">
+        <div class="<?php echo ($category['b_enabled'] == 1 ? 'enabled' : 'disabled'); ?>" category_id="<?php echo $category['pk_i_id']; ?>">
             <div class="d-flex flex-row-horizontal bg-light rounded-1 shadow-sm">
                 <div class="px-2 border-end handle"><i class="align-middle bi bi-arrows-move" role="button"></i></div>
                 <div class="px-2 border-end">
@@ -402,23 +352,19 @@ function drawCategory($category)
                         echo '<span class="align-middle toggle bi bi-chevron-right"></span>';
                     } else {
                         echo '<span class="align-middle toggle bi bi-chevron-down"></span>';
-                    }
-                    ?>
+                    } ?>
                 </div>
                 <div class="px-2 name-cat" id="<?php echo 'quick_edit_' . $category['pk_i_id']; ?>">
                     <?php echo '<span class="align-middle name">' . $category['s_name'] . '</span>'; ?>
                 </div>
                 <div class="px-2 ms-auto btn-group">
                     <a class="btn btn-sm" onclick="show_iframe('content_list_<?php echo $category['pk_i_id']; ?>','<?php echo
-                    $category['pk_i_id']; ?>');" title="<?php _e('Edit'); ?>"><i class="bi bi-pencil-fill"></i></a>
-                    <a class="btn btn-sm enable"
-                       onclick="enable_cat('<?php echo $category['pk_i_id']; ?>')"
-                    <?php if ($category['b_enabled'] == 1) {
-                        echo 'title="' . __('Disable') . '"><i class="bi bi-slash-circle-fill"></i></a>';
-                    } else {
-                        echo 'title="' . __('Enable') . '"><i class="bi bi-play-circle-fill"></i></a>';
-                    } ?>
-                    <a class="btn btn-sm" onclick="delete_category(<?php echo $category['pk_i_id']; ?>)" title="<?php _e('Delete'); ?>">
+                                                                                                                    $category['pk_i_id']; ?>');" title="<?php _e('Edit'); ?>"><i class="bi bi-pencil-fill"></i></a>
+                    <a class="btn btn-sm enable" onclick="enable_cat('<?php echo $category['pk_i_id']; ?>')" <?php if ($category['b_enabled'] == 1) {
+                                                                                                                    echo 'title="' . __('Disable') . '"><i class="bi bi-slash-circle-fill"></i></a>';
+                                                                                                                } else {
+                                                                                                                    echo 'title="' . __('Enable') . '"><i class="bi bi-play-circle-fill"></i></a>';
+                                                                                                                } ?> <a class="btn btn-sm" onclick="delete_category(<?php echo $category['pk_i_id']; ?>)" title="<?php _e('Delete'); ?>">
                         <i class="text-danger bi bi-x-circle-fill"></i>
                     </a>
                 </div>
@@ -433,7 +379,7 @@ function drawCategory($category)
             </ul>
         <?php } ?>
     </li>
-    <?php
+<?php
 } //End drawCategory
 ?>
 <?php osc_current_admin_theme_path('parts/header.php'); ?>
@@ -462,22 +408,71 @@ function drawCategory($category)
     </div>
     <!-- /categories form -->
 </div>
-<!-- /right container -->
-<div id="dialog-delete-category" title="<?php echo osc_esc_html(__('Delete category')); ?>"
-     class="has-form-actions hide" data-category-id="">
-    <div class="form-horizontal">
-        <div class="form-row">
-            <?php _e('<strong>WARNING</strong>: This will also delete the listings under that category. This action cannot be undone. Are you sure you want to continue?'); ?>
-        </div>
-        <div class="form-actions">
-            <div class="wrapper">
-                <a class="btn btn-dim" href="javascript:void(0);"
-                   onclick="$('#dialog-delete-category').dialog('close');"><?php _e('Cancel'); ?></a>
-                <a id="category-delete-submit" href="javascript:void(0);"
-                   class="btn btn-red"><?php echo osc_esc_html(__('Delete')); ?></a>
-                <div class="clear"></div>
+<!-- right container -->
+<div id="deleteModal" method="get" action="<?php echo osc_admin_base_url(true); ?>" class="modal fade static">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    <?php echo __('Delete category'); ?>
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <?php _e('<strong>WARNING</strong>: This will also delete the listings under that category.'
+                    . ' This action cannot be undone. Are you sure you want to continue?');
+                ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal"><?php _e('Cancel'); ?></button>
+                <button id="deleteSubmit" data-bs-dismiss="modal" class="btn btn-sm btn-red" type="submit">
+                    <?php echo __('Delete'); ?>
+                </button>
             </div>
         </div>
     </div>
 </div>
+<script>
+    function delete_category(id) {
+        var deleteModal = document.getElementById('deleteModal');
+        deleteModal.setAttribute('data-category-id', id);
+        (new bootstrap.Modal(document.getElementById('deleteModal'))).toggle();
+        return false;
+    }
+
+    document.getElementById("deleteSubmit").onclick = function() {
+        let categoryId = document.getElementById(
+            "deleteModal"
+        ).dataset.categoryId;
+        let url = "<?php
+                    echo osc_admin_base_url(true); ?>?page=ajax&action=delete_category&<?php echo osc_csrf_token_url();
+                                                                                        ?>&id=" + categoryId;
+        fetch(url, {
+                credentials: "same-origin"
+            }).then(function(response) {
+                if (!response.ok) {
+                    setJsMessage("error", response.statusText);
+                }
+                return response.json()
+            })
+            .then(function(jsonObj) {
+                if (jsonObj.error) {
+                    setJsMessage("error", jsonObj.error);
+                }
+                if (jsonObj.ok) {
+                    setJsMessage("ok", jsonObj.ok);
+                    document.getElementById('list_' + categoryId).remove()
+                }
+            }).catch(function(error) {
+                setJsMessage("error", "<?php echo osc_esc_js(__("Ajax error, try again.")); ?>:" + error);
+            });
+    };
+
+    function setJsMessage(alertClass, alertMessage) {
+        var pTag = document.getElementById("jsMessage").querySelector("p");
+        pTag.setAttribute("class", alertClass);
+        pTag.textContent = alertMessage;
+        document.getElementById("jsMessage").classList.remove('hide')
+    }
+</script>
 <?php osc_current_admin_theme_path('parts/footer.php'); ?>
