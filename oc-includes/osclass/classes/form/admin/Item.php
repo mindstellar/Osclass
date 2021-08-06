fd83a1233699cf6b86b736b5826568de73a24d48
navjottomer <navjottomer@gmail.com>
2021-08-07 02:05:02 +0530

    Style: media setting page, using bootstrap color picker now.

---
+++ oc-includes/osclass/classes/form/admin/Item.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Mindstellar Community.
+ * User: navjottomer
+ * Date: 06-08-2021
+ * Time: 16:03
+ * License is provided in root directory.
+ */
+
+namespace mindstellar\form\admin;
+
+class Item
+{
+
+}
