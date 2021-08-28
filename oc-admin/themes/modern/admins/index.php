08b817212137a7e128d909fbb9f87f2fe4b32475
navjottomer <navjottomer@gmail.com>
2021-08-28 19:46:55 +0530

    Admin page datatable improvement

--- oc-admin/themes/modern/admins/index.php
+++ oc-admin/themes/modern/admins/index.php
@@ -31,9 +31,9 @@
 function addHelp()
 {
     echo '<p>'
-         . __('Add users who can manage your page. You can add admins or moderators: '
-              . 'admins have access to the whole admin panel while moderators can only modify listings and see stats.')
-         . '</p>';
+        . __('Add users who can manage your page. You can add admins or moderators: '
+            . 'admins have access to the whole admin panel while moderators can only modify listings and see stats.')
+        . '</p>';
 }
 
 
@@ -54,15 +54,12 @@ osc_add_filter('admin_title', 'customPageTitle');
 
 function customPageHeader()
 {
-    ?>
+?>
     <h1><?php _e('Admins'); ?>
-        <a class="ms-1 bi bi-question-circle-fill float-right" data-bs-target="#help-box" data-bs-toggle="collapse"
-           href="#help-box"></a>
-        <a href="<?php echo osc_admin_base_url(true); ?>?page=admins&amp;action=add"
-           class="ms-1 text-success float-end" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php _e('Add admin'); ?>"><i
-                    class="bi bi-plus-circle-fill"></i></a>
+        <a class="ms-1 bi bi-question-circle-fill float-right" data-bs-target="#help-box" data-bs-toggle="collapse" href="#help-box"></a>
+        <a href="<?php echo osc_admin_base_url(true); ?>?page=admins&amp;action=add" class="ms-1 text-success float-end" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php _e('Add admin'); ?>"><i class="bi bi-plus-circle-fill"></i></a>
     </h1>
-    <?php
+<?php
 }
 
 osc_add_hook('admin_page_header', 'customPageHeader');
@@ -71,38 +68,48 @@ $iDisplayLength = __get('iDisplayLength');
 $aData          = __get('aAdmins');
 
 osc_current_admin_theme_path('parts/header.php'); ?>
-    <h2 class="render-title"><?php _e('Manage admins'); ?></h2>
-    <div class="relative">
-        <form class="" id="datatablesForm" action="<?php echo osc_admin_base_url(true); ?>" method="post">
-            <input type="hidden" name="page" value="admins"/>
-            <div id="bulk-actions">
-                <div class="input-group input-group-sm">
-                    <?php osc_print_bulk_actions('bulk_actions', 'action', __get('bulk_options'),
-                                                 'select-box-extra form-select'); ?>
-                    <input type="submit" id="bulk_apply" class="btn btn-primary" value="<?php echo osc_esc_html(__('Apply')); ?>"/>
-                </div>
+<h2 class="render-title"><?php _e('Manage admins'); ?></h2>
+<div class="relative">
+    <form class="" id="datatablesForm" action="<?php echo osc_admin_base_url(true); ?>" method="post">
+        <input type="hidden" name="page" value="admins" />
+        <div id="bulk-actions">
+            <div class="input-group input-group-sm">
+                <?php osc_print_bulk_actions(
+                    'bulk_actions',
+                    'action',
+                    __get('bulk_options'),
+                    'select-box-extra form-select'
+                ); ?>
+                <input type="submit" id="bulk_apply" class="btn btn-primary" value="<?php echo osc_esc_html(__('Apply')); ?>" />
             </div>
-            <div class="table-contains-actions shadow-sm">
-                <table class="table" cellpadding="0" cellspacing="0">
-                    <thead>
+        </div>
+        <div class="table-contains-actions shadow-sm">
+            <table class="table" cellpadding="0" cellspacing="0">
+                <thead>
                     <tr class="table-secondary">
-                        <th class="col-bulkactions"><input id="check_all" type="checkbox"/></th>
+                        <th class="col-bulkactions"><input id="check_all" type="checkbox" /></th>
                         <th><?php _e('Username'); ?></th>
                         <th><?php _e('Name'); ?></th>
                         <th><?php _e('E-mail'); ?></th>
                     </tr>
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
                     <?php if (count($aData['aaData']) > 0) { ?>
                         <?php foreach ($aData['aaData'] as $array) { ?>
                             <tr>
                                 <?php foreach ($array as $key => $value) { ?>
-                                    <?php if ($key == 0) { ?>
-                                        <td class="col-bulkactions">
-                                    <?php } else { ?>
-                                        <td>
-                                    <?php } ?>
-                                    <?php echo $value; ?>
+                                    <td <?php if ($key == 0) {
+                                            echo 'class="col-bulkactions"';
+                                        } elseif ($key == 1) {
+                                            echo ' echo data-col-name="' . __('Username') . '"';
+                                        } elseif ($key == 2) {
+                                            echo ' data-col-name="' . __('Name') . '"';
+                                        } elseif ($key == 3) {
+                                            echo ' data-col-name="' . __('E-mail') . '"';
+                                        } else {
+                                            echo  'data-col-name="' . ucfirst($key) . '"';
+                                        } ?>>
+                                        <?php echo $value; ?>
                                     </td>
                                 <?php } ?>
                             </tr>
@@ -114,117 +121,116 @@ osc_current_admin_theme_path('parts/header.php'); ?>
                             </td>
                         </tr>
                     <?php } ?>
-                    </tbody>
-                </table>
-                <div id="table-row-actions"></div><!-- used for table actions -->
-            </div>
-        </form>
-    </div>
+                </tbody>
+            </table>
+            <div id="table-row-actions"></div><!-- used for table actions -->
+        </div>
+    </form>
+</div>
 <?php
 function showingResults()
 {
     $aData = __get('aAdmins');
     echo '<ul class="showing-results"><li><span>'
-         . osc_pagination_showing((Params::getParam('iPage') - 1)
-                                  * $aData['iDisplayLength'] + 1,
-                                  ((Params::getParam('iPage') - 1) * $aData['iDisplayLength'])
-                                  + count($aData['aaData']),
-                                  $aData['iTotalDisplayRecords']) . '</span></li></ul>';
+        . osc_pagination_showing((Params::getParam('iPage') - 1)
+                * $aData['iDisplayLength'] + 1,
+            ((Params::getParam('iPage') - 1) * $aData['iDisplayLength'])
+                + count($aData['aaData']),
+            $aData['iTotalDisplayRecords']
+        ) . '</span></li></ul>';
 }
 
 
 osc_add_hook('before_show_pagination_admin', 'showingResults');
 osc_show_pagination_admin($aData);
 ?>
-    <form id="deleteModal" method="get" action="<?php echo osc_admin_base_url(true); ?>"
-          class="modal fade static">
-        <input type="hidden" name="page" value="admins"/>
-        <input type="hidden" name="action" value="delete"/>
-        <input type="hidden" name="id[]" value=""/>
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">
-                        <?php _e('Delete admin'); ?>
-                    </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <?php _e('Are you sure you want to delete this admin?'); ?>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal"><?php _e('Cancel'); ?></button>
-                    <button id="deleteSubmit" class="btn btn-sm btn-red" type="submit">
-                        <?php echo __('Delete'); ?>
-                    </button>
-                </div>
+<form id="deleteModal" method="get" action="<?php echo osc_admin_base_url(true); ?>" class="modal fade static">
+    <input type="hidden" name="page" value="admins" />
+    <input type="hidden" name="action" value="delete" />
+    <input type="hidden" name="id[]" value="" />
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    <?php _e('Delete admin'); ?>
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-        </div>
-    </form>
-    <div id="bulkActionsModal" class="modal fade static" tabindex="-1" aria-labelledby="bulkActionsModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="bulkActionsModalLabel"><?php _e('Bulk actions'); ?></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p></p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal"><?php _e('Cancel'); ?></button>
-                    <button id="bulkActionsSubmit" onclick="bulkActionsSubmit()"
-                            class="btn btn-sm btn-red"><?php echo osc_esc_html(__('Delete')); ?></button>
-                </div>
+            <div class="modal-body">
+                <?php _e('Are you sure you want to delete this admin?'); ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal"><?php _e('Cancel'); ?></button>
+                <button id="deleteSubmit" class="btn btn-sm btn-red" type="submit">
+                    <?php echo __('Delete'); ?>
+                </button>
             </div>
         </div>
     </div>
-    <script>
-        function delete_dialog(id) {
-            var deleteModal = document.getElementById('deleteModal')
-            deleteModal.querySelector('input[name=\'id[]\']').value = id;
-            (new bootstrap.Modal(document.getElementById('deleteModal'))).toggle()
-            return false;
-        }
+</form>
+<div id="bulkActionsModal" class="modal fade static" tabindex="-1" aria-labelledby="bulkActionsModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="bulkActionsModalLabel"><?php _e('Bulk actions'); ?></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal"><?php _e('Cancel'); ?></button>
+                <button id="bulkActionsSubmit" onclick="bulkActionsSubmit()" class="btn btn-sm btn-red"><?php echo osc_esc_html(__('Delete')); ?></button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function delete_dialog(id) {
+        var deleteModal = document.getElementById('deleteModal')
+        deleteModal.querySelector('input[name=\'id[]\']').value = id;
+        (new bootstrap.Modal(document.getElementById('deleteModal'))).toggle()
+        return false;
+    }
 
-        function toggleBulkActionsModal() {
-            var bulkSelect = document.getElementById('bulk_actions')
-            var bulkActionsModal = new bootstrap.Modal(document.getElementById('bulkActionsModal'))
-            if (bulkSelect.options[bulkSelect.selectedIndex].value !== '') {
-                bulkActionsModal.toggle()
-            }
-            event.preventDefault()
-            return false
+    function toggleBulkActionsModal() {
+        var bulkSelect = document.getElementById('bulk_actions')
+        var bulkActionsModal = new bootstrap.Modal(document.getElementById('bulkActionsModal'))
+        if (bulkSelect.options[bulkSelect.selectedIndex].value !== '') {
+            bulkActionsModal.toggle()
         }
+        event.preventDefault()
+        return false
+    }
 
-        function bulkActionsSubmit() {
-            document.getElementById('datatablesForm').submit()
-        }
+    function bulkActionsSubmit() {
+        document.getElementById('datatablesForm').submit()
+    }
 
-        document.getElementById('datatablesForm').onsubmit = function () {
-            toggleBulkActionsModal()
-        };
-        var bulkActionsModal = document.getElementById('bulkActionsModal')
-        bulkActionsModal.addEventListener('show.bs.modal', function () {
-            var bulkSelect = document.getElementById('bulk_actions')
-            bulkActionsModal.querySelector('.modal-body p').textContent = bulkSelect.options[bulkSelect.selectedIndex]
-                .getAttribute('data-dialog-content')
-            bulkActionsModal.querySelector('#bulkActionsSubmit').textContent = bulkSelect.options[bulkSelect.selectedIndex].text;
-        })
-    </script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            // check_all bulkactions
-            $("#check_all").change(function () {
-                var isChecked = $(this).prop("checked");
-                $('.col-bulkactions input').each(function () {
-                    if (isChecked == 1) {
-                        this.checked = true;
-                    } else {
-                        this.checked = false;
-                    }
-                });
+    document.getElementById('datatablesForm').onsubmit = function() {
+        toggleBulkActionsModal()
+    };
+    var bulkActionsModal = document.getElementById('bulkActionsModal')
+    bulkActionsModal.addEventListener('show.bs.modal', function() {
+        var bulkSelect = document.getElementById('bulk_actions')
+        bulkActionsModal.querySelector('.modal-body p').textContent = bulkSelect.options[bulkSelect.selectedIndex]
+            .getAttribute('data-dialog-content')
+        bulkActionsModal.querySelector('#bulkActionsSubmit').textContent = bulkSelect.options[bulkSelect.selectedIndex].text;
+    })
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // check_all bulkactions
+        $("#check_all").change(function() {
+            var isChecked = $(this).prop("checked");
+            $('.col-bulkactions input').each(function() {
+                if (isChecked == 1) {
+                    this.checked = true;
+                } else {
+                    this.checked = false;
+                }
             });
         });
-    </script>
+    });
+</script>
 <?php osc_current_admin_theme_path('parts/footer.php'); ?>
