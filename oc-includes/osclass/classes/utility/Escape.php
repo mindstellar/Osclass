9bcd7c4dc53d20fe9d7629672455bed7da8f3113
codefactor-io <support@codefactor.io>
2021-08-30 11:56:34 +0000

    [CodeFactor] Apply fixes to commit fdc8529
    [ci skip] [skip ci]

--- oc-includes/osclass/classes/utility/Escape.php
+++ oc-includes/osclass/classes/utility/Escape.php
@@ -59,7 +59,7 @@ class Escape
     {
         return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
             return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
-        },                           $str);
+        }, $str);
     }
 
     /**
@@ -84,7 +84,7 @@ class Escape
     {
         return preg_replace_callback('/&#([0-9a-zA-Z]*);/', function ($match) {
             return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
-        },                           $str);
+        }, $str);
     }
 
     /**
