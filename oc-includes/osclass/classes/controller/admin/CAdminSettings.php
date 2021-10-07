51dec1069cf34f4fb9a9315ee3f13f4553c6ea3a
Defected <53878055+dftd@users.noreply.github.com>
2021-10-07 20:30:14 +0200

    Cookies & GDPR

--- oc-includes/osclass/classes/controller/admin/CAdminSettings.php
+++ oc-includes/osclass/classes/controller/admin/CAdminSettings.php
@@ -83,6 +83,10 @@ class CAdminSettings
             case ('latestsearches_post'):
                 $do = new CAdminSettingsLatestSearches();
                 break;
+            case ('cookiesgdpr'):
+            case ('cookiesgdpr_post'):
+                $do = new CAdminSettingsCookiesGDPR();
+                break;
             case ('update'):
             case ('check_updates'):
             default:
