79e7cb8b7c66fd0f6ebf2a89f50afdb08d85ad65
Navjot Tomer <navjottomer@gmail.com>
2024-02-06 13:16:31 +0530

    Add installation finish and target pages Fixed few php warnings

---
+++ oc-includes/osclass/installer/gui/install-target.php
@@ -0,0 +1,99 @@
+<?php if (!defined('ABS_PATH')) {
+    exit('ABS_PATH is not loaded. Direct access is not allowed.');
+}
+
+$internet_error = false;
+require_once LIB_PATH . 'osclass/helpers/hUtils.php';
+$country_list = osc_file_get_contents(osc_get_locations_json_url());
+$country_list = json_decode($country_list, false);
+$country_list = $country_list->locations;
+
+$country_ip = '';
+if (preg_match(
+    '|([a-z]{2})-([A-Z]{2})|',
+    Params::getServerParam('HTTP_ACCEPT_LANGUAGE'),
+    $match
+)) {
+    $country_ip = $match[2];
+}
+
+if (!isset($country_list[0]->s_country_name)) {
+    $internet_error = true;
+}
+?>
+<form class="p-3" id="target_form" name="target_form" action="#" method="post" onsubmit="return false;">
+    <h2 class="display-6"><?php _e('Information needed'); ?></h2>
+    <div class="form-table">
+        <h4 class="title"><?php _e('Admin user'); ?></h4>
+        <div class="admin-user mb-3">
+            <div class="row mb-3">
+                <label class="col-md-3 col-sm-6 col-form-label" for="admin_user"><?php _e('Username'); ?></label>
+                <div class="col-md-4 col-sm-6">
+                    <input class="form-control" size="25" id="admin_user" name="s_name" type="text" value="admin" />
+                    <span id="admin-user-error" class="error" aria-hidden="true" style="display:none;"><?php _e('Admin user is required'); ?></span>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-md-3 col-sm-6 col-form-label" for="s_passwd"><?php _e('Password'); ?></label>
+                <div class="col-md-4 col-sm-6">
+                    <input size="25" class=" form-control password_test" name="s_passwd" id="s_passwd" type="password" value="" autocomplete="off" />
+                </div>
+                <td></td>
+            </div>
+        </div>
+        <div class="admin-user mb-3">
+            <?php _e('A password will be automatically generated for you if you leave this blank.'); ?>
+            <i class="bi bi-question-circle-fill vtip" title="<?php echo osc_esc_html(__('You can modify username and password if you like, just change the input value.')); ?>">
+            </i>
+        </div>
+        <h4 class="title"><?php _e('Contact information'); ?></h4>
+        <div class="contact-info">
+            <div class="row mb-3">
+                <label class="col-md-3 col-sm-6 col-form-label" for="webtitle"><?php _e('Web title'); ?></label>
+                <div class="col-md-4 col-sm-6"><input class="form-control" type="text" id="webtitle" name="webtitle" size="25" /></div>
+                <td></td>
+            </div>
+            <div class="row mb-3">
+                <label class="col-md-3 col-sm-6 col-form-label" for="email"><?php _e('Contact e-mail'); ?></label>
+                <div class="col-md-4 col-sm-6">
+                    <input class="form-control" type="text" id="email" name="email" size="25" />
+                    <span id="email-error" class="error" style="display:none;"><?php _e('Put your e-mail here'); ?></span>
+                </div>
+                <span id="email-error" class="error" style="display:none;"><?php _e('Put your e-mail here'); ?></span>
+            </div>
+        </div>
+        <h4 class="title"><?php _e('Location'); ?></h4>
+        <p class="space-left-25 left no-bottom"><?php _e('Choose a country where your target users are located'); ?>
+            .</p>
+        <div id="location">
+            <?php if (!$internet_error) { ?>
+                <input type="hidden" id="skip-location-input" name="skip-location-input" value="<?php echo $country_ip ? 0 : 1; ?>" />
+                <div class="col-md-3 col-sm-6" id="country-box">
+                    <select class="form-select" name="location-json" id="location-json">
+                        <option value="skip"><?php _e("Skip location"); ?></option>
+                        <?php foreach ($country_list as $c) : ?>
+                            <option value="<?php echo $c->s_file_name; ?>" <?php echo ($country_ip && strpos($c->s_file_name, $country_ip) === 0) ? 'selected="selected"' : ''; ?>>
+                                <?php echo $c->s_country_name; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            <?php } else { ?>
+                <div id="location-error">
+                    <div class="alert alert-danger">
+                        <?php _e('No internet connection. You can continue the installation and insert countries later.'); ?>
+                    </div>
+                    <input type="hidden" id="skip-location-input" name="skip-location-input" value="1" />
+                </div>
+            <?php }; ?>
+        </div>
+    </div>
+    <div class="mt-3">
+        <a href="#" class="btn btn-primary" onclick="validate_form();">Next</a>
+    </div>
+</form>
+<div id="lightbox" style="display:none;">
+    <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+    </div>
+</div>
---
+++ oc-includes/osclass/installer/gui/install.php
@@ -0,0 +1,204 @@
+<?php if( !defined('ABS_PATH') ){ exit('ABS_PATH is not loaded. Direct access is not allowed.'); } ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php _e('Osclass Installation'); ?></title>
+    <link rel="stylesheet" type="text/css" media="all"
+          href="<?php echo get_absolute_url(); ?>oc-includes/assets/bootstrap/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" media="all"
+          href="<?php echo get_absolute_url(); ?>oc-includes/assets/bootstrap-icons/bootstrap-icons.css"/>
+    <link rel="stylesheet" type="text/css" media="all"
+          href="<?php echo get_absolute_url(); ?>oc-includes/osclass/installer/vtip/css/vtip.css"/>
+
+    <script src="<?php echo get_absolute_url(); ?>oc-includes/assets/jquery/jquery.min.js"
+            type="text/javascript"></script>
+    <script src="<?php echo get_absolute_url(); ?>oc-includes/assets/jquery-ui/jquery-ui.min.js"
+            type="text/javascript"></script>
+    <script src="<?php echo get_absolute_url(); ?>oc-includes/assets/bootstrap/bootstrap.min.js"
+            type="text/javascript"></script>
+    <script src="<?php echo get_absolute_url(); ?>oc-includes/osclass/installer/vtip/vtip.js"
+            type="text/javascript"></script>
+    <script src="<?php echo get_absolute_url(); ?>oc-includes/osclass/installer/install.js"
+            type="text/javascript"></script>
+</head>
+<body>
+<div id="wrapper" class="container-md">
+    <div class="row">
+        <div class="offset-md-1 col-md-10 col-sm-12 align-self-center p-5" id="container">
+            <div class="card rounded-3" tabindex="-1">
+                <div id="header" class="card-header text-dark bg-light installation">
+                    <div class="text-center">
+                        <img width="350" src="<?php echo get_absolute_url(); ?>oc-includes/images/osclass-logo.png"
+                             alt="Osclass"
+                             title="Osclass"/>
+                    </div>
+                    <?php if (in_array($step, array(2, 3))) { ?>
+                        <?php if ($step === 2) {
+                            $databaseStep = 'text-info';
+                            $targetStep   = 'text-muted';
+                        } elseif ($step === 3) {
+                            $databaseStep = 'text-muted';
+                            $targetStep   = 'text-info';
+                        } ?>
+                        <ul class="nav nav-pills nav-fill justify-content-center">
+                            <li class="nav-item border-bottom">
+                                <div class="nav-link <?php echo $databaseStep; ?>"><strong>1 - Database</strong></div>
+                            </li>
+                            <li class="nav-item border-bottom">
+                                <div class="nav-link <?php echo $targetStep; ?>"><strong>2 - Target</strong></div>
+                            </li>
+                        </ul>
+                    <?php } ?>
+                </div>
+                <div class="card-body bg-light" id="content">
+                    <?php if ($step === 1) { ?>
+                        <h2 class="card-title text-center display-6"><?php _e('Welcome'); ?></h2>
+                        <?php if (isset($error) && $error) { ?>
+                            <div class="alert alert-danger shadow-sm" role="alert">
+                                <h4><?php _e('Oops! You need a compatible Hosting'); ?></h4>
+                                <?php _e('Your hosting seems to be not compatible, check your settings.'); ?>
+                            </div>
+                            <br>
+                        <?php } ?>
+
+                        <form class="p-3" action="install.php" method="post">
+                            <input type="hidden" name="step" value="2"/>
+                            <div class="form-table">
+                                <?php if (count($jsonLocales) > 1) { ?>
+                                    <div>
+                                        <div class="row mb-3">
+                                            <label for="install_locale" class="col-md-3 col-sm-6
+                                                col-form-label"><strong><?php _e('Choose language'); ?></strong></label>
+                                            <div class="col-md-3 col-sm-6">
+                                                <select class="form-control"
+                                                        aria-label="<?php _e('Choose language'); ?>" id="install_locale"
+                                                        name="install_locale"
+                                                        onchange="window.location.href='?install_locale='+document.getElementById(this.id).value">
+                                                    <?php foreach ($jsonLocales as $k => $locale) { ?>
+                                                        <option value="<?php echo osc_esc_html($k); ?>" <?php if ($k
+                                                            === Session::newInstance()->_get('userLocale')
+                                                        ) {
+                                                            echo 'selected="selected"';
+                                                                       } ?>><?php echo $locale; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                                <?php if ($error) { ?>
+                                    <p><?php _e('Check the next requirements:'); ?></p>
+                                    <div class="requirements_help alert alert-warning shadow-sm">
+                                        <span class="small"><b><?php _e('Requirements help:'); ?></b></span>
+                                        <ul>
+                                            <?php foreach ($requirements as $k => $v) { ?>
+                                                <?php if (!$v['fn'] && $v['solution'] != '') { ?>
+                                                    <li class="small"><?php echo $v['solution']; ?></li>
+                                                <?php } ?>
+                                            <?php } ?>
+                                            <li class="small">
+                                                <a href="https://github.com/mindstellar/Osclass/discussions" target="_blank"
+                                                   hreflang="en"><?php _e('Need more help?'); ?></a></li>
+                                        </ul>
+                                    </div>
+                                <?php } else { ?>
+                                    <div class="alert alert-success shadow-sm"><?php _e('All right! All the requirements have met:'); ?></div>
+                                <?php } ?>
+                                <ul>
+                                    <?php foreach ($requirements as $k => $v) { ?>
+                                        <li><?php echo $v['requirement']; ?> <i
+                                                    class="bi <?php echo $v['fn'] ? 'text-success bi-check'
+                                                        : 'text-danger bi-x-circle-fill'; ?>"></i></li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
+                            <?php if ($error) { ?>
+                                <p class="margin20">
+                                    <input type="button" class="btn btn-primary"
+                                           onclick="document.location = 'install.php?step=1'"
+                                           value="<?php echo osc_esc_html(__('Try again')); ?>"/>
+                                </p>
+                            <?php } else { ?>
+                                <p class="margin20">
+                                    <input type="submit" class="btn btn-primary"
+                                           value="<?php echo osc_esc_html(__('Run the install')); ?>"/>
+                                </p>
+                            <?php } ?>
+                        </form>
+                    <?php } elseif ($step == 2) {
+                        display_database_config();
+                    } elseif ($step === 3) {
+                        if (!isset($error['error'])) {
+                            display_target();
+                        } else {
+                            display_database_error($error, $step - 1);
+                        }
+                    } elseif ($step === 4) {
+                        // ping engines
+
+                        if (isset($_COOKIE['osclass_ping_engines'])) {
+                            try {
+                                ping_search_engines($_COOKIE['osclass_ping_engines']);
+                            } catch (Exception $e) {
+                                LogOsclassInstaller::newInstance()
+                                    ->error($e->getMessage(), $e->getFile() . ' at line: '
+                                        . $e->getLine());
+                            }
+                        }
+
+                        setcookie('osclass_save_stats', '', time() - 3600);
+                        setcookie('osclass_ping_engines', '', time() - 3600);
+
+                        // copy robots.txt
+                        $source      = LIB_PATH . 'osclass/installer/robots.txt';
+                        $destination = ABS_PATH . 'robots.txt';
+                        if (function_exists('copy')) {
+                            @copy($source, $destination);
+                        } else {
+                            $contentx   = @file_get_contents($source);
+                            $openedfile = fopen($destination, 'wb');
+                            fwrite($openedfile, $contentx);
+                            fclose($openedfile);
+                            $status = true;
+                            if ($contentx === false) {
+                                $status = false;
+                            }
+                        }
+                        display_finish($password);
+
+                        // Install bender theme for first time.
+                        if (!is_dir(CONTENT_PATH.'themes/bender')) {
+                            $fileSystem = new \mindstellar\utility\FileSystem();
+                            $bender_filename       = 'bender.zip';
+                            $download_path   = CONTENT_PATH . 'downloads/';
+                            if ($downloaded = $fileSystem->downloadFile(
+                                'https://github.com/mindstellar/theme-bender/releases/download/v3.2.3/bender_3.2.3.zip',
+                                $download_path . 'bender.zip'
+                            )
+                            ) {
+                                $zip = new \mindstellar\utility\Zip();
+                                $resultCode =$zip->unzipFile($downloaded, CONTENT_PATH . 'themes/');
+                                $fileSystem->remove($downloaded);
+                            }
+                        }
+                    }
+                    ?>
+                </div>
+                <div class="card-footer" id="footer">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="https://docs.mindstellar.com/osclass-docs/"
+                                                        target="_blank"
+                                                        hreflang="en"><?php _e('Documentation'); ?></a></li>
+                        <li class="list-inline-item"><a href="https://github.com/mindstellar/Osclass/" target="_blank"
+                                                        hreflang="en"><?php _e('Feedback'); ?></a></li>
+                        <li class="list-inline-item"><a href="https://github.com/mindstellar/Osclass/discussions" target="_blank"
+                                                        hreflang="en"><?php _e('Forums'); ?></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
