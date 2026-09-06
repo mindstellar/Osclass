a5c2056732559bb83b2902b8d575c980df93745c
navjottomer <navjottomer@gmail.com>
2026-09-06 11:53:48 +0000

    users: fix the settings checkboxes laying out side by side
    Hand-written form-controls divs made the three toggles flex siblings across the row.
    Group them in one form row through the helper and open the form with osc_admin_form_open.

--- oc-admin/themes/modern/users/settings.php
+++ oc-admin/themes/modern/users/settings.php
@@ -21,58 +21,45 @@ osc_admin_page(array(
 ));
 
 osc_current_admin_theme_path('parts/header.php'); ?>
-    <!-- settings form -->
     <?php osc_admin_page_head(__('User Settings')); ?>
-    <form action="<?php echo osc_admin_base_url(true); ?>" method="post">
-        <input type="hidden" name="page" value="users"/>
-        <input type="hidden" name="action" value="settings_post"/>
-        <fieldset>
-            <div class="form-horizontal">
-                <?php osc_admin_form_row_open(__('Settings')); ?>
-                        <?php osc_admin_checkbox(array(
-                            'name'    => 'enabled_users',
-                            'id'      => 'enabled_users',
-                            'label'   => __('Users enabled'),
-                            'checked' => osc_users_enabled(),
-                        )); ?>
-                    </div>
-                    <div class="form-controls separate-top-medium">
-                        <?php osc_admin_checkbox(array(
-                            'name'    => 'enabled_user_registration',
-                            'id'      => 'enabled_user_registration',
-                            'label'   => __('Anyone can register'),
-                            'checked' => osc_user_registration_enabled(),
-                        )); ?>
-                    </div>
-                    <div class="form-controls separate-top-medium">
-                        <?php osc_admin_checkbox(array(
-                            'name'    => 'enabled_user_validation',
-                            'id'      => 'enabled_user_validation',
-                            'label'   => __('Users need to validate their account'),
-                            'checked' => osc_user_validation_enabled(),
-                        )); ?>
-                <?php osc_admin_form_row_close(); ?>
-                <?php osc_admin_form_row_open(__('Admin notifications')); ?>
-                        <?php osc_admin_checkbox(array(
-                            'name'    => 'notify_new_user',
-                            'id'      => 'notify_new_user',
-                            'label'   => __('When a new user is registered'),
-                            'checked' => osc_notify_new_user(),
-                        )); ?>
-                <?php osc_admin_form_row_close(); ?>
-                <?php osc_admin_text(array(
-                    'id'    => 'username_blacklist',
-                    'name'  => 'username_blacklist',
-                    'label' => __('Username blacklist'),
-                    'value' => osc_username_blacklist(),
-                    'width' => 'key',
-                    'help'  => __('List of terms not allowed in usernames, separated by commas'),
+    <?php osc_admin_form_open(array('page' => 'users', 'action' => 'settings_post')); ?>
+        <?php osc_admin_form_row_open(__('Settings')); ?>
+                <?php osc_admin_checkbox(array(
+                    'name'    => 'enabled_users',
+                    'id'      => 'enabled_users',
+                    'label'   => __('Users enabled'),
+                    'checked' => osc_users_enabled(),
+                ));
+                osc_admin_checkbox(array(
+                    'name'    => 'enabled_user_registration',
+                    'id'      => 'enabled_user_registration',
+                    'label'   => __('Anyone can register'),
+                    'checked' => osc_user_registration_enabled(),
+                ));
+                osc_admin_checkbox(array(
+                    'name'    => 'enabled_user_validation',
+                    'id'      => 'enabled_user_validation',
+                    'label'   => __('Users need to validate their account'),
+                    'checked' => osc_user_validation_enabled(),
                 )); ?>
-                <?php osc_admin_form_actions(array(
-                    array('label' => __('Save changes'), 'type' => 'submit', 'variant' => 'primary', 'attrs' => array('id' => 'save_changes')),
+        <?php osc_admin_form_row_close(); ?>
+        <?php osc_admin_form_row_open(__('Admin notifications')); ?>
+                <?php osc_admin_checkbox(array(
+                    'name'    => 'notify_new_user',
+                    'id'      => 'notify_new_user',
+                    'label'   => __('When a new user is registered'),
+                    'checked' => osc_notify_new_user(),
                 )); ?>
-            </div>
-        </fieldset>
-    </form>
-    <!-- /settings form -->
+        <?php osc_admin_form_row_close(); ?>
+        <?php osc_admin_text(array(
+            'id'    => 'username_blacklist',
+            'name'  => 'username_blacklist',
+            'label' => __('Username blacklist'),
+            'value' => osc_username_blacklist(),
+            'width' => 'key',
+            'help'  => __('List of terms not allowed in usernames, separated by commas'),
+        )); ?>
+    <?php osc_admin_form_close(array(
+        array('label' => __('Save changes'), 'type' => 'submit', 'variant' => 'primary', 'attrs' => array('id' => 'save_changes')),
+    )); ?>
 <?php osc_current_admin_theme_path('parts/footer.php'); ?>
